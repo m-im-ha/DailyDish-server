bd3f6d7bee21f071c15b2ef65adb48d7256f15b0
imran <sherlockholmesad221@gmail.com>
2024-12-22 20:59:43 +0600

    add foodSchema

---
+++ src/modules/food/food.model.js
@@ -0,0 +1,85 @@
+const mongoose = require('mongoose');
+
+const foodSchema = new mongoose.Schema(
+  {
+    foodName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    foodImage: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (v) {
+          return /^(https?:\/\/.*\.(?:png|jpg|jpeg|gif|svg))$/.test(v);
+        },
+        message: 'Invalid image URL format!',
+      },
+    },
+    foodQuantity: {
+      type: Number,
+      required: true,
+      min: [1, 'Quantity must be at least 1'],
+    },
+    pickupLocation: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    expiredDate: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: function (v) {
+          return v > new Date();
+        },
+        message: 'Expired date must be in the future!',
+      },
+    },
+    additionalNotes: {
+      type: String,
+      trim: true,
+    },
+    donator: {
+      image: {
+        type: String,
+        required: true,
+        validate: {
+          validator: function (v) {
+            return /^(https?:\/\/.*\.(?:png|jpg|jpeg|gif|svg))$/.test(v);
+          },
+          message: 'Invalid image URL format!',
+        },
+      },
+      name: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        trim: true,
+        validate: {
+          validator: function (v) {
+            return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(v);
+          },
+          message: 'Invalid email format!',
+        },
+      },
+    },
+    foodStatus: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Food = mongoose.model('Food', foodSchema);
+
+module.exports = Food;
