4d4f81ddea1e259a59e319794dff77554c853777
imran <sherlockholmesad221@gmail.com>
2024-12-24 14:25:42 +0600

    Add auth route for login and logout

---
+++ src/route/auth.route.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { generateToken } = require("../utils/jwt");
+const router = express.Router();
+
+// Login Route
+router.post("/login", async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const token = generateToken({ email });
+
+    // Set token in HTTP-only cookie
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "Strict",
+        maxAge: 24 * 60 * 60 * 1000,
+      })
+      .status(200)
+      .json({ message: "Login successful!" });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to login." });
+  }
+});
+
+// Logout Route
+router.post("/logout", (req, res) => {
+  res.clearCookie("token").status(200).json({ message: "Logged out!" });
+});
+
+module.exports = router;
