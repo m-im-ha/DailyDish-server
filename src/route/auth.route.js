d5f6b618cf9ad59c119c5493c5b85de4f46e5f1f
imran <sherlockholmesad221@gmail.com>
2024-12-24 16:12:06 +0600

    Add route for google-login

--- src/route/auth.route.js
+++ src/route/auth.route.js
@@ -7,8 +7,8 @@ router.post("/login", async (req, res) => {
   const { email } = req.body;
 
   try {
+    // Generate JWT token
     const token = generateToken({ email });
-
     // Set token in HTTP-only cookie
     res
       .cookie("token", token, {
@@ -23,6 +23,26 @@ router.post("/login", async (req, res) => {
     res.status(500).json({ message: "Failed to login." });
   }
 });
+
+// Google Login Route
+router.post("/google-login", async (req, res) => {
+    const { email } = req.body;
+  
+    try {
+      const token = generateToken({ email });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: "Strict",
+          maxAge: 24 * 60 * 60 * 1000,
+        })
+        .status(200)
+        .json({ message: "Google Login successful!" });
+    } catch (error) {
+      res.status(500).json({ message: "Failed to login with Google." });
+    }
+  });
 
 // Logout Route
 router.post("/logout", (req, res) => {
