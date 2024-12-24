9772a8b985701d39c85ccb2ceba26408d3b58ad7
imran <sherlockholmesad221@gmail.com>
2024-12-24 12:15:14 +0600

    Add delete request to delete food

--- src/route/foods.route.js
+++ src/route/foods.route.js
@@ -3,28 +3,28 @@ const Food = require("../modules/food/food.model");
 const router = express.Router();
 
 // Get all foods
-router.get("/availablefoods", async (req,res)=>{
-    try{
-        const foods = await Food.find();
-        res.status(200).json(foods);
-    }catch(error){
-        console.error("Error to get foods.",error.message);
-        res.status(500).json({message : "Failed to fetch foods."})
-    }
-})
+router.get("/availablefoods", async (req, res) => {
+  try {
+    const foods = await Food.find();
+    res.status(200).json(foods);
+  } catch (error) {
+    console.error("Error to get foods.", error.message);
+    res.status(500).json({ message: "Failed to fetch foods." });
+  }
+});
 
 // Get specific food details
-router.get("/foodDetails/:id", async(req,res)=>{
-  try{
+router.get("/foodDetails/:id", async (req, res) => {
+  try {
     // console.log(`req-params :::::::: `,req.params);
-    const {id}= req.params;
+    const { id } = req.params;
     const food = await Food.findById(id);
     res.status(200).json(food);
-  }catch(error){
-    console.error("Error to get food details : ",error.message);
-    res.status(500).json({message : "Failed to fetch food details."})
+  } catch (error) {
+    console.error("Error to get food details : ", error.message);
+    res.status(500).json({ message: "Failed to fetch food details." });
   }
-})
+});
 
 // Add food
 router.post("/addfood", async (req, res) => {
@@ -36,6 +36,21 @@ router.post("/addfood", async (req, res) => {
   } catch (error) {
     console.error("Error to add food", error.message);
     res.status(500).json({ message: "Failed to creat food." });
+  }
+});
+
+// Delete specific food when requested
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const {id} = req.params;
+    const food = await Food.findByIdAndDelete(id);
+    if(!food){
+      res.status(404).json({message : "Food not found."});
+    }
+    res.status(200).json({message: "Food deleted successfully."})
+  } catch (error) {
+    console.error(`Error to delete food.`, error.message);
+    res.status(500).json({message : "Failed to delete food."})
   }
 });
 
